21c9351eed0b3056de14c7999eaabff889b08468
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-17 16:32:09 -0700

    updated System definition

--- ISystem.hx
+++ ISystem.hx
@@ -1,5 +1,13 @@
 package edge;
 
-interface ISystem<T> {
-  public function process(components : T) : Void;
+/**
+`ISystem` implementation requires either;
+
+public function update(a : CompA, b : CompB ...) : Void;
+
+// this needs to be reviewed:
+public function updateAll(list : Array<Array<Dynamic>>) : Void;
+*/
+interface ISystem {
+  function getRequirements() : Array<Class<Dynamic>>;
 }
