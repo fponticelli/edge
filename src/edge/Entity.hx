a6b7cc87710e798d644f9d4f2d47a7434a8455ce
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-19 22:17:23 -0700

    changed type of components in Entity

--- src/edge/Entity.hx
+++ src/edge/Entity.hx
@@ -4,51 +4,51 @@ using thx.core.Arrays;
 
 @:access(edge.World)
 class Entity {
-  var components : Map<String, Dynamic>;
+  var components : Map<String, {}>;
   public var world(default, null) : World;
-  public function new(?components : Array<Dynamic>) {
+  public function new(?components : Array<{}>) {
     this.components = new Map();
     if(null != components)
       addMany(components);
   }
 
-  public function add(component : Dynamic) {
+  public function add(component : {}) {
     _add(component);
     if(null != world)
       world.matchSystems(this);
   }
 
-  public function addMany(components : Array<Dynamic>) {
+  public function addMany(components : Array<{}>) {
     components.pluck(_add(_));
     if(null != world)
       world.matchSystems(this);
   }
 
-  public function exists(component : Dynamic)
+  public function exists(component : {})
     return existsType(key(component));
 
   public function existsType(type : String)
     return components.exists(type);
 
-  public function remove(component : Dynamic) {
+  public function remove(component : {}) {
     _remove(component);
     if(null != world)
       world.matchSystems(this);
   }
 
-  public function removeMany(components : Array<Dynamic>) {
+  public function removeMany(components : Array<{}>) {
     components.pluck(_remove(_));
     if(null != world)
       world.matchSystems(this);
   }
 
-  public function removeType(type : Class<Dynamic>) {
+  public function removeType(type : Class<{}>) {
     _removeTypeName(Type.getClassName(type));
     if(null != world)
       world.matchSystems(this);
   }
 
-  public function removeTypes(types : Array<Class<Dynamic>>) {
+  public function removeTypes(types : Array<Class<{}>>) {
     types.pluck(_removeTypeName(Type.getClassName(_)));
     if(null != world)
       world.matchSystems(this);
@@ -57,14 +57,14 @@ class Entity {
   inline public function iterator()
     return components.iterator();
 
-  function _add(component : Dynamic) {
+  function _add(component : {}) {
     var type = key(component);
     if(components.exists(type))
       remove(components.get(type));
     components.set(type, component);
   }
 
-  function _remove(component : Dynamic) {
+  function _remove(component : {}) {
     var type = key(component);
     _removeTypeName(type);
   }
@@ -72,6 +72,6 @@ class Entity {
   function _removeTypeName(type : String)
     components.remove(type);
 
-  inline function key(component : Dynamic)
+  inline function key(component : {})
     return Type.getClassName(Type.getClass(component));
 }
