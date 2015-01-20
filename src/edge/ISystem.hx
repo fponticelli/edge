a2f994409dda6d7b60d1e6f1528e698b3f4acda9
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-19 22:53:47 -0700

    some notes

--- src/edge/ISystem.hx
+++ src/edge/ISystem.hx
@@ -4,6 +4,15 @@ package edge;
 `ISystem` implementation requires either;
 
 public function update(a : CompA, b : CompB ...) : Void;
+
+optional fields:
+  `entity : Entity` // sets the current entity being updated
+  `entities : Array<T>` // array of components objects where each
+                        // object contains the fields returned by
+                        // `getEntitiesRequirements()` plus an
+                        // `entity : Entity` field
+TODO
+  `delta : Float` // for elapsed time since last update
 */
 interface ISystem {
   function getUpdateRequirements() : Array<Class<Dynamic>>;
