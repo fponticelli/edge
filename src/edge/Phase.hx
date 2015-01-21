6b2a80839d6b559a16bb54daac105a7137759f17
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-20 22:23:17 -0700

    added Phase implementation

---
+++ src/edge/Phase.hx
@@ -0,0 +1,128 @@
+package edge;
+
+@:access(edge.NodeSystem)
+class Phase {
+  var first : NodeSystem;
+  var last : NodeSystem;
+  var mapSystem : Map<ISystem, NodeSystem>;
+  var mapType : Map<String, ISystem>;
+  public function new() {
+    mapSystem = new Map();
+    mapType = new Map();
+  }
+
+  public function add(system : ISystem) {
+    remove(system);
+    var node = createNode(system);
+    if(null == first) {
+      first = node;
+      last = node;
+    } else {
+      node.prev = last;
+      last.next = node;
+      last = node;
+    }
+  }
+
+  public function insertBefore(ref : ISystem, system : ISystem) {
+    var noderef = mapSystem.get(ref);
+    if(null == noderef)
+      throw 'Phase.insertBefore: unable to find $ref system';
+    var node = createNode(system);
+    if(noderef == first) {
+      node.next = noderef;
+      noderef.prev = node;
+      first = node;
+    } else {
+      var prev = noderef.prev;
+      prev.next = node;
+      node.prev = prev;
+      node.next = noderef;
+      noderef.prev = node;
+    }
+  }
+
+  public function insertAfter(ref : ISystem, system : ISystem) {
+    var noderef = mapSystem.get(ref);
+    if(null == noderef)
+      throw 'Phase.insertAfter: unable to find $ref system';
+    var node = createNode(system);
+    if(noderef == last) {
+      node.prev = noderef;
+      noderef.next = node;
+      last = node;
+    } else {
+      var next = noderef.next;
+      next.prev = node;
+      node.next = next;
+      node.prev = noderef;
+      noderef.next = node;
+    }
+  }
+
+  public function remove(system : ISystem) {
+    var node = mapSystem.get(system);
+    mapType.remove(key(system));
+    if(null == node)
+      return;
+    mapSystem.remove(system);
+    if(node == first && node == last) {
+      first = last = null;
+    } else if(node == first) {
+      first = node.next;
+      node.next.prev = null;
+    } else if(node == last) {
+      first = node.prev;
+      node.prev.next = null;
+    } else {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+    }
+  }
+
+  public function removeType(cls : Class<Dynamic>) {
+    var system = mapType.get(Type.getClassName(cls));
+    if(null == system)
+      throw 'type system ${Type.getClassName(cls)} is not included in this Phase';
+    return remove(system);
+  }
+
+  public function iterator()
+    return new NodeSystemIterator(first);
+
+  function createNode(system : ISystem) {
+    var node = new NodeSystem(system);
+    mapSystem.set(system, node);
+    mapType.set(key(system), system);
+    return node;
+  }
+
+  function key(system : ISystem)
+    return Type.getClassName(Type.getClass(system));
+}
+
+class NodeSystem {
+  public var system(default, null) : ISystem;
+  public var next(default, null) : NodeSystem;
+  public var prev(default, null) : NodeSystem;
+
+  public function new(system : ISystem) {
+    this.system = system;
+  }
+}
+
+class NodeSystemIterator {
+  var node : NodeSystem;
+  public function new(node : NodeSystem) {
+    this.node = node;
+  }
+
+  public function hasNext()
+    return null != node;
+
+  public function next() {
+    var system = node.system;
+    node = node.next;
+    return system;
+  }
+}
