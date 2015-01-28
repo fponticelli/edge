8adb1f30265f0bfb52c52c752b152ae0c0697ee6
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-01-27 22:37:59 -0700

    added example

---
+++ demo/basic/src/Game.hx
@@ -0,0 +1,80 @@
+import edge.*;
+import minicanvas.MiniCanvas;
+
+class Game {
+  public static var width(default, null) = 200;
+  public static var height(default, null) = 200;
+
+  public static function main() {
+    var mini = MiniCanvas.create(width, height)
+                 .display("basic example"),
+        world = new World();
+
+    for(i in 0...300)
+      world.engine.add(new Entity([
+          new Position(
+            Math.random() * width,
+            Math.random() * height),
+          new Velocity(
+            Math.random() * 2 - 1,
+            Math.random() * 2 - 1)
+        ]));
+
+    for(i in 0...20)
+      world.engine.add(new Entity([
+          new Position(
+            Math.random() * width,
+            Math.random() * height)
+        ]));
+
+    world.physics.add(new UpdateMovement());
+
+    world.render.add(new RenderBackground(mini));
+    world.render.add(new RenderDots(mini));
+
+    world.start();
+  }
+}
+
+class Position implements IComponent {
+  var x : Float;
+  var y : Float;
+}
+
+class Velocity implements IComponent {
+  var vx : Float;
+  var vy : Float;
+}
+
+class RenderDots implements ISystem {
+  var mini : MiniCanvas;
+  public function new(mini : MiniCanvas)
+    this.mini = mini;
+
+  function update(pos : Position)
+    mini.dot(pos.x, pos.y, 2, 0x000000FF);
+}
+
+class RenderBackground implements ISystem {
+  var mini : MiniCanvas;
+  public function new(mini : MiniCanvas)
+    this.mini = mini;
+
+  function update()
+    mini.clear();
+}
+
+class UpdateMovement implements ISystem {
+  function update(pos : Position, vel : Velocity) {
+    var dx = pos.x + vel.vx,
+        dy = pos.y + vel.vy;
+    if(dx <= 0 && vel.vx < 0 || dx >= Game.width && vel.vx > 0)
+      vel.vx = -vel.vx;
+    else
+      pos.x = dx;
+    if(dy <= 0 && vel.vy < 0 || dy >= Game.height && vel.vy > 0)
+      vel.vy = -vel.vy;
+    else
+      pos.y = dy;
+  }
+}
