03acf88e0173147b76b2e757bd625c55629f5def
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-03 08:40:22 -0700

    moved out SystemInfo

--- src/edge/Engine.hx
+++ src/edge/Engine.hx
@@ -160,22 +160,3 @@ class Engine {
     return comps.length == requirements.length ? comps : null;
   }
 }
-
-typedef SystemInfo = {
-  hasComponents : Bool,
-  hasDelta : Bool,
-  hasEngine : Bool,
-  hasEntity : Bool,
-  hasEntities : Bool,
-  hasBefore : Bool,
-  before : Dynamic,
-  update : Dynamic,
-  phase : Phase,
-  components : Map<Entity, Array<Dynamic>>,
-  entities : View<Dynamic>/*,
-  views : Map<String, {
-    requirements : Void -> Array<Class<Dynamic>>,
-    view : View<Dynamic>
-  }
-  */
-}
