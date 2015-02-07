f9125db8e42b9fe239119bfa81fc73bf47ba1fc1
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-06 20:31:34 -0700

    support collection of entities

--- src/edge/SystemInfo.hx
+++ src/edge/SystemInfo.hx
@@ -7,14 +7,18 @@ class SystemInfo {
   public var hasDelta(default, null) : Bool;
   public var hasEngine(default, null) : Bool;
   public var hasEntity(default, null) : Bool;
-  public var hasEntities(default, null) : Bool;
   public var hasBefore(default, null) : Bool;
   public var phase(default, null) : Phase;
   public var before(default, null) : Dynamic;
   public var update(default, null) : Dynamic;
   public var components(default, null) : Map<Entity, Array<Dynamic>>;
-  public var entities(default, null) : View<Dynamic>;
   public var system(default, null) : ISystem;
+
+  public var collections(default, null) : Map<String, {
+    classes : Array<Class<Dynamic>>,
+    fields : Array<String>,
+    view : View<Dynamic>
+  }>;
 
   public function new(system : ISystem, phase : Phase) {
     this.system        = system;
@@ -23,12 +27,21 @@ class SystemInfo {
     this.hasEngine     = hasField(system, "engine");
     this.hasEntity     = hasField(system, "entity");
     this.hasBefore     = hasField(system, "before");
-    this.hasEntities   = null != system.entityRequirements;
     this.update        = Reflect.field(system, "update");
     this.phase         = phase;
     this.before        = null;
     this.components    = new Map();
-    this.entities      = new View();
+
+    this.collections = new Map();
+    if(null != system.entityRequirements) {
+      var view = new View();
+      this.collections.set("entities", {
+        classes : system.entityRequirements.pluck(_.cls),
+        fields : system.entityRequirements.pluck(_.name),
+        view : view
+      });
+      Reflect.setField(system, "entities", view);
+    }
 
     if(hasBefore)
       before = Reflect.field(system, "before");
@@ -37,10 +50,3 @@ class SystemInfo {
   static function hasField(o : {}, field : String)
     return Type.getInstanceFields(Type.getClass(o)).contains(field);
 }
-
-/*,
-views : Map<String, {
-  requirements : Void -> Array<Class<Dynamic>>,
-  view : View<Dynamic>
-}
-*/
