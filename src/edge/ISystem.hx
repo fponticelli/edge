3fc3c76036f0b333e21f0daab88322139ea0d7a6
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-08 11:21:38 -0700

    cleanup

--- src/edge/ISystem.hx
+++ src/edge/ISystem.hx
@@ -1,23 +1,6 @@
 package edge;
 
-/**
-`ISystem` implementation requires either;
-
-public function update(a : CompA, b : CompB ...) : Void;
-
-optional fields:
-  `entity : Entity` // sets the current entity being updated
-  `entities : Array<T>` // array of components objects where each
-                        // object contains the fields returned by
-                        // `getEntitiesRequirements()` plus an
-                        // `entity : Entity` field
-TODO
-  `delta : Float` // for elapsed time since last update
-*/
 @:autoBuild(edge.macro.BuildSystem.complete())
 interface ISystem {
   private var __systemProcess : ISystemProcess;
-//  private function __getSystemProcess(engine : Engine) : ISystemProcess;
-//  var componentRequirements(default, null) : Array<Class<Dynamic>>;
-//  var entityRequirements(default, null) : Array<{ name : String , cls : Class<Dynamic> }>;
 }
--- src/edge/ISystemProcess.hx
+++ src/edge/ISystemProcess.hx
@@ -1,15 +1,7 @@
 package edge;
 
 interface ISystemProcess {
-//  public function before() : Void;
-//  public var hasUpdateItems : Bool;
-
   public function update(engine : Engine, delta : Float) : Void;
-
   public function addEntity(entity : Entity) : Void;
   public function removeEntity(entity : Entity) : Void;
-
-// TODO remove
-//  public function setEntity(entity : Entity) : Void
-//  public var collections : Map<String, ViewInfo>;
 }
