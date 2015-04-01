3f5142e83e999de4cd9751d754ce2dab0b863179
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:08:46 -0600

    exclude hidden files from zip

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 rm edge.zip
-zip -r edge.zip src test demo extraParams.hxml haxelib.json LICENSE README.md
+zip -r edge.zip src test demo extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit edge.zip
